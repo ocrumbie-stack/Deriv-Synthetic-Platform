338175ab17f8a50229f2888f648cf81411550f91
Oneil Crumbie <on.crumbie@gmail.com>
2026-09-11 22:00:02 -0700

    Set TradingView account target

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
 
     deriv_app_id: str = ""
     deriv_api_token: str = ""
-    deriv_account_id: str = ""
+    deriv_account_id: str = "1483409"
     deriv_ws_url: str = "wss://ws.derivws.com/websockets/v3"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
